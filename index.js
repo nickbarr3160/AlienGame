cece6322b530cba1faf05200bdde2914aafa4602
Nick Barr <nbarr2@my.bcit.ca>
2022-03-20 15:45:06 -0700

    Fixing alien source path

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-//work done by Nick and Izzy
-
 var steps = 1
 var alien = document.getElementById("al");
 var defaultPos = alien.style.top
@@ -21,6 +19,8 @@ lTimer = null
 boxTimer = null
 
 
+// Jump Alien
+
 function jump ()
 {   
     alien.style.top = parseInt(alien.style.top)+ -10 + "px"
@@ -32,6 +32,10 @@ function jump ()
         },750) 
     }
 }
+
+
+// Function to avoid double jumping
+
 function noJump ()
 {
     alien.style.top = parseInt(alien.style.top)+ 15 + "px"
@@ -41,21 +45,28 @@ function noJump ()
     }
 }
 
- function walkingStep(){
+// Function for alien steps
+
+function walkingStep ()
+{
     if (steps > 30)
     {
         steps = 0
     }
+
     alien.src = "./alien 2/alienrwalk" + steps + ".gif"
 }
 
-
+// Alien walk right
         
-        function rightWalk (){
-           walkingStep()
-           if (alien.style.top == "200px") 
+function rightWalk ()
+        {
+                walkingStep()
+           if (alien.style.top == "200px")
+            
             {
                 steps++
+              
             } 
             else if (alien.style.top != "200px")
             {
@@ -65,51 +76,66 @@ function noJump ()
 
                if (alien.style.left == screenEnd)
                {
-                leftTimer = setInterval (moveLeft, 20)
-                clearInterval(rightTimer)
-                rTimer = null
+                  leftTimer = setInterval (moveLeft, 20)
+                  clearInterval(rightTimer)
+                  rTimer = null
                }
 
                if (alien.style.left == parseInt(smallBox.style.left) + -60 + "px" && jumpTimer == null)
                {
                    boxTimer = setInterval(kickBox,20)
-                   setTimeout (function(){
-                        clearInterval(boxTimer)
+                   setTimeout (function()
+                   {
+                            clearInterval(boxTimer)
                    },200)
                 }                   
-               }
+        }
                
-               function kickBox (){
-                   smallBox.style.left = parseInt(smallBox.style.left)+ 15 + "px"
-                }   
+    
+    // Function for kicking small box
+    function kickBox ()
+    {
+    smallBox.style.left = parseInt(smallBox.style.left)+ 15 + "px"
+    }   
                    
                
         
- function walkingStepTwo (){
-            if (steps > 30)
+    function walkingStepTwo ()
+    {
+        if (steps > 30)
             {
                 steps = 0
             }
             alien.src = "./alien 2/alienlwalk" + steps + ".gif"
         }
 
-        function leftWalk (){
+        
+        
+    // Alien walk right
+    
+    function leftWalk ()
+        {
             walkingStepTwo ()
             if (alien.style.top == "200px")
                 {
                  steps++
                 } 
+               
              else if (alien.style.top != "200px")
                 {
                  steps
                 }
+                
                 alien.style.left = parseInt(alien.style.left)+ -1 + "px"
+                
                 if (alien.style.left <= "-10px")
                 {
                    rTimer = setInterval (rightWalk, 20)
                    clearInterval(lTimer)
                    lTimer = null
                 }
+
+
 
                 if(alien.style.left == parseInt(bigBox.style.left)+ 35 + "px" )
                 {
@@ -120,13 +146,14 @@ function noJump ()
         }
 
             
+        //Alien Jump
 
-
-        function jumpStop (){
+        function jumpStop ()
+        {
             if (foggle == true)
             {
                clearInterval(jumpTimer)
-               
+        
                 jumpTimer = setInterval(noJump, 100)
                 foggle = null
                 setTimeout(function()
@@ -142,61 +169,60 @@ function noJump ()
             }
         }
  
-    
+        //Alien controls on press
 
-        function makeJump (keyBeingPressed)
-        {
+        function onPress (keyBeingPressed)
+        {   
+            // jump on up arrow
             if(keyBeingPressed.which == 38)
+            
             {   
+               
                     if (jumpTimer == null)
                     {
                         jumpTimer = setInterval(jump, 100)
                         foggle = true                    
                     }
                 setTimeout(jumpStop, 500)
+            
+               
+               
                 console.log(alien.style.top)
             }
-            
-           else if (keyBeingPressed.which == 39)
+           
+           // walk right on right arrow 
+            else if (keyBeingPressed.which == 39)
+    
             {
                 if (rTimer == null)
-                {     
+                {  
                  rTimer = setInterval(rightWalk, 20)
                  clearInterval(lTimer)
                  lTimer = null
-                 
                 }
             }
                  
-                
-               
-
-            
-            
+            //walk left on left arrow    
             else if (keyBeingPressed.which == 37)
             {
                 if (lTimer == null)
                 {
-                    lTimer = setInterval(leftWalk, 20)
-                    clearInterval(rTimer)
-                    rTimer = null 
+                lTimer = setInterval(leftWalk, 20)
+                clearInterval(rTimer)
+                rTimer = null    
                 }
-
-                
-    
             }
             
+            // stop alien on down arrow
             else if (keyBeingPressed.which == 40)
             {
                 clearInterval(rTimer)
-                    rTimer = null
+                rTimer = null
                 clearInterval(lTimer)
-                    lTimer = null
-            }
-
-            
+                lTimer = null
+            }    
         }
         
                 
         
-window.onkeydown = makeJump
+window.onkeydown = onPress
